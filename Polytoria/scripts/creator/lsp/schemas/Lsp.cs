f04b69955cb3fdd2cbecc78ef3b3208c6cfefd40
Zander Polk <zanderpolk@gmail.com>
2026-07-25 19:45:35 -0400

    Request runtime configuration from Luau LSP

--- Polytoria/scripts/creator/lsp/schemas/Lsp.cs
+++ Polytoria/scripts/creator/lsp/schemas/Lsp.cs
@@ -1,7 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -46,6 +42,9 @@ public class LspInitializationOptions
 {
 	[JsonPropertyName("respect_editor_formatting_options")]
 	public bool? RespectEditorFormattingOptions { get; set; } // Used by StyLua
+
+	[JsonPropertyName("changeConfiguration")]
+	public bool? ChangeConfiguration { get; set; }
 }
 
 public class LspClientCapabilities
